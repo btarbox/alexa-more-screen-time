ef38dabf0bf39a22f1dcea86723900e4faa9fe67
Brian Tarbox <tarbox@cabotresearch.com>
2017-09-02 21:17:01 -0400

    added more chores; close to full refactored functionality

--- ChoreTIme.js
+++ ChoreTIme.js
@@ -1,10 +1,13 @@
 var languageStrings = {
     'en': {
         'translation': {
-            'WELCOME' : "Welcome to Chore Time, ",
-            'HELP'    : " this is the help for chore time ",
-            'ABOUT'   : "chores are cool.",
-            'STOP'    : "Okay, see you next time!"
+            'WELCOME'  : "Welcome to More Screen Time, lets see if your child has done their chores<break time='750ms'/> ",
+            'MINI_HELP': "Welcome to More Screen Time, Answer yes, no or they don't have to after each question about your child's chore<break time='500ms'/> ",
+            'HELP'     : "We get chores based on morning, afternoon or weekend and ask if your child has done them.  You say yes or no to each chore.  If your child has done all of" +
+            " their chores they are allowed more screentime, otherwise not.  You can also say, they don't have to.  This means the chore isn't required and we will no longer" +
+            " ask you about it.  You can also say, reset, to get back to the full list of chores<break time='750ms'/> ",
+            'ABOUT'    : "More Screen Time makes a child control their own screen time.",
+            'STOP'     : "Okay, see you next time!"
         }
     }
     // , 'de-DE': { 'translation' : { 'TITLE'   : "Local Helfer etc." } }
@@ -23,6 +26,8 @@ var current_day = date.getDay();
 var dynamoChoresName = '';
 var dynamoChores = '';
 var todChores = '';
+var choreIndexesToRemove = '';
+var invocation_count = 0;
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -34,8 +39,18 @@ exports.handler = function(event, context, callback) {
 
 var handlers = {
     'LaunchRequest': function () {
-        var say = this.t('WELCOME') + this.t('HELP');
-        console.log("at launch request, checking dynamoDB for last result: " +  this.attributes['lastChoreResult']);
+        // var say = this.t('WELCOME') + this.t('HELP');
+        invocation_count = this.attributes['invocation_count'];
+        if (typeof invocation_count == "undefined") {
+            console.log('invocation_count was not found in dynamo, initializing it to 1');
+            this.attributes['invocation_count'] = 1;
+            invocation_count = 1;
+        } else {
+            invocation_count += 1;
+            this.attributes['invocation_count'] = invocation_count;
+            console.log("invocation_count incremented to " + invocation_count);
+        }
+        console.log("at launch request, checking dynamoDB for last result: " +  this.attributes['lastChoreResult'] + " and invocation count:" + invocation_count);
         choreIndex = 0;  // not sure why I need this here but w/o it choreIndex start == 1
         getChoreListAndWelcomeUser(this);
     },
@@ -56,54 +71,108 @@ var handlers = {
         handleScreenTimeDenied(this);
     },
 
-    'AMAZON.HelpIntent': function () {
-        this.emit(':ask', this.t('HELP'));
+    'DontHaveToIntent': function () {  // user said the chore doesn't apply to them
+        handleSaidDontHaveTo(this);
     },
+
+    'ResetChoresIntent': function () {
+        handleResetChores(this);
+    },
+
+    'AMAZON.HelpIntent': function () {
+        choreIndex -= 1;  // repeat the existing chore question
+        askAboutAChore(this.t('HELP'), this)
+        // this.emit(':ask', this.t('HELP'));
+    },
+
     'AMAZON.CancelIntent': function () {
         this.emit(':tell', this.t('STOP'));
     },
     'AMAZON.StopIntent': function () {
         this.emit(':tell', this.t('STOP'));
+    },
+
+    'Unhandled': function() {
+        console.log('got to the unhandled state');
+        this.emit(':tell', this.t('STOP'));
     }
 };
+
+function handleResetChores(emmiter) {
+    console.log("at handleResetChores...not yet implemented");
+    emmiter.emit(':tell', this.t('STOP'));
+}
 
 function handleSaidYes(emmiter) {
     console.log("at handleSaidYes");
     if(choreIndex >= maxChores) {
         handleScreenTimeAllowed(emmiter);
     } else {
-        askAboutAChore(emmiter);
+        askAboutAChore("", emmiter);
+    }
+}
+
+// this is the same as handleSaidYes except that we also remember this is a chore to skip forever
+function handleSaidDontHaveTo(emmiter) {
+    var removeIndex = choreIndex - 1;
+    choreIndexesToRemove += removeIndex + ',';
+    console.log("at handleSaidDontHaveTo; remove chore:" + removeIndex + " " + chores[removeIndex] + "; choreIndexesToRemove:" + choreIndexesToRemove);
+    if(choreIndex >= maxChores) {
+        handleScreenTimeAllowed(emmiter);
+    } else {
+        askAboutAChore("", emmiter);
     }
 }
 
 function handleScreenTimeAllowed(emmiter) {
-    var choreAttribute = dynamoChoresName; //'weekendchores';
-    console.log("at handleScreenTimeAllowed, storing chores in dynamo:" + choreAttribute);
+    var choreAttribute = dynamoChoresName;
+    console.log("at handleScreenTimeAllowed," + choreIndexesToRemove + ", storing chores in dynamo:" + choreAttribute);
     emmiter.attributes['lastChoreResult'] = 'allowed';
-    emmiter.attributes[choreAttribute] = chores;
+    emmiter.attributes['invocation_count'] = invocation_count;
+    stripChores();
+    ///emmiter.attributes[choreAttribute] = chores;
     var say = "<p>Your child may have more screen time</p>";
     emmiter.emit(':tell', say);
 }
+
 function handleScreenTimeDenied(emmiter) {
-    var choreAttribute = dynamoChoresName; //'weekendchores';
-    console.log("at handleScreenTimeDenied, storing chores in dynamo:" + choreAttribute);
+    var choreAttribute = dynamoChoresName;
+    console.log("at handleScreenTimeDenied," + choreIndexesToRemove + "  storing chores in dynamo:" + choreAttribute);
     emmiter.attributes['lastChoreResult'] = 'denied';
-    emmiter.attributes[choreAttribute] = chores;
+    emmiter.attributes['invocation_count'] = invocation_count;
+
+    stripChores();
+    ////emmiter.attributes[choreAttribute] = chores;
     var say = "<p>Your child has not completed all their chores so they may not have more screen time</p>";
     emmiter.emit(':tell', say);
 }
 
-function askAboutAChore(emmiter) {
-    console.log("at askAboutAChore:" + choreIndex);
+function askAboutAChore(preamble, emmiter) {
+    console.log("at askAboutAChore:" + choreIndex + " of " + maxChores + " preamble:"+preamble);
     var nextChore = chores[choreIndex];
     choreIndex = choreIndex + 1;
 
-    var say = 'Have they ' + nextChore;
+    var say = preamble + 'Have they ' + nextChore;
     emmiter.emit(':ask', say, 'next prompt');
 }
 
 
 // ----------------------- utility methods -----------------------
+
+function stripChores() {
+    var choresRemovedSoFar = 0;
+    console.log("about to strip I dont have to chores; original chorelist: " + chores + " choreIndexesToRemove:" + choreIndexesToRemove);
+    var ignored = choreIndexesToRemove.split(",").map(function (index) {
+        if(index.length > 0) {
+            console.log("about to remove " + index + " from chorelist");
+            var indexNum = Number(index) - choresRemovedSoFar;
+            choresRemovedSoFar += 1;
+            chores.splice(indexNum, 1);
+            console.log("chores now:" + chores);
+        }
+        return 42;  // not sure if I need this just for syntax
+    });
+}
 
 function getTimeOfDay() {
     var tod = "";
@@ -140,60 +209,55 @@ function getChoreListAndWelcomeUser(emmiter) {
         console.log("did not find existing chore list for this user");
     } else {
         chores = dynamoChores;
-        MAX_CHORE = chores.length + 1;
-        console.log("found existing chore list for this user:" + dynamoChores + " going directly to askAboutAChore with " + MAX_CHORE + " chores");
-        askAboutAChore(emmiter);
+        maxChores = chores.length;
+        console.log("found existing chore list for this user:" + dynamoChores + " going directly to askAboutAChore with " + maxChores + " chores");
+        askAboutAChore(buildWelcome(emmiter), emmiter);
         return;
     }
 
     console.log("got time of day file: " + todChores);
-    var hashed = '1234'; //require('crypto').createHash('md5').update(session.user.userId).digest('hex');
-    console.log("user id hashed " + hashed);
     const bucket = "lambdaeventsource";
     var keyBase = "MoreScreenTime/";
     var keyDefaultUser = "Default";
     const key = keyBase + keyDefaultUser + todChores;
-    const keyExistingUser = keyBase + hashed + "/" + todChores;
-    console.log("key is " + key + ", keyExistingUser is " + keyExistingUser);
+    console.log("key is " + key);
 
-    const s3ExistingUserParams = {
-        Bucket: bucket,
-        Key: keyExistingUser
-    };
     const s3NewUserParams = {
         Bucket: bucket,
         Key: key
     };
 
-    console.log("about to get s3 object with chore list for existing user, if not found then look for default chores");
-    s3.getObject(s3ExistingUserParams, function(err, data) {
+    console.log("about to get s3 object with chore list for default chores");
+    s3.getObject(s3NewUserParams, function(err, data) {
         if (err) {
-            console.log("chores not found for existing user " + err + ", try for default user");
-            s3.getObject(s3NewUserParams, function(err, data) {
-                if (err) {
-                    console.log("chores not found for new user");
-                } else {
-                    console.log("got chores for default user");
-                    getChoresFromFileAndWelcomeUser(emmiter, data, keyDefaultUser);
-                }
-            });
+            console.log("chores not found for default user " + err);
         } else {
-            getChoresFromFileAndWelcomeUser(emmiter, data, keyExistingUser);
+            getChoresFromFileAndWelcomeUser(emmiter, data, key);
         }
     });
 }
 
-// called twice from getChoreList, once for specific user and once for default
+// called from getChoreList, for default user
 function getChoresFromFileAndWelcomeUser(emmiter, data, whichUser) {
     var body = data.Body.toString('ascii');
     var chorelist = body.split(",");
-    console.log("about to getChoresFromFile for user " + whichUser + ", first get tz for user");
+    console.log("got ChoresFromFile for user " + whichUser );
 
     console.log("about to parse first chore in getChoresFromFile " + chorelist[0] + " dayOfWeek " + current_day);
     var extraTime = 30
     chorelist.shift();
     chores = chorelist;
     console.log("finished parse, got " + extraTime);
-    MAX_CHORE = chorelist.length;
-    askAboutAChore(emmiter);
+    maxChores = chorelist.length;
+    askAboutAChore(buildWelcome(emmiter), emmiter);
+}
+
+function buildWelcome(emmiter) {
+    var percent = Math.random() * 12;  // a random number between 0 and 12
+    console.log('at buildWelcome, percent:' + percent + " invocation_count:" + invocation_count);
+    if (invocation_count < percent) {
+        return emmiter.t('MINI_HELP');
+    } else {
+        return emmiter.t('WELCOME');
+    }
 }
